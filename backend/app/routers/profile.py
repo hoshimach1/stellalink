7779cac9c6c33184cb8c126509938fc95e99c064
hoshimach1 <alex0svnh@gmail.com>
2026-03-19 01:42:13 +0300

    fix

--- backend/app/routers/profile.py
+++ backend/app/routers/profile.py
@@ -1,6 +1,6 @@
 from uuid import UUID
 
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database import get_db
